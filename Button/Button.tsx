4230f929f458251287b710ef6c969749ed50b2d1
pereirafrederic <pereira.frederic.developpeur@gmail.com>
2019-06-03 21:51:08 +0200

    2 files modified

--- Button/Button.tsx
+++ Button/Button.tsx
@@ -28,11 +28,18 @@ const Button = memo(
       <div className="Button__container-out">
         <div className="Button__container-in">
 
-          <div className="Button__container__icon">
-            <Icon className="mdi-icon" path={icon} />
-          </div>
-
           <MediaQuery minDeviceWidth={1224} >
+            <div className="Button__container__icon tooltip">
+              <Icon className="mdi-icon" path={icon} />
+              
+            </div>
+            <span className="tooltiptext">{label}</span>
+          </MediaQuery>
+
+          <MediaQuery maxDeviceWidth={1224} >
+              <div className="Button__container__icon">
+              <Icon className="mdi-icon" path={icon} />
+            </div>
             <div className="Button__container__label">{label}</div>
           </MediaQuery>
         </div>
